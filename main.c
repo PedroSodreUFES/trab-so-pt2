fb740b0bda3ce42f15adfc8345c53dc10da90112
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-03-23 19:24:13 -0300

    toma ai tutu

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 #define N 4  // Número de produtores
-#define MAX_ORDERS 12  // Número total de ordens (4 produtores x 3 ordens por produtor)
+#define MAX_ORDERS 4  // Número total de ordens (4 produtores x 3 ordens por produtor)
 
 typedef struct {
     char nome_paciente[100];
